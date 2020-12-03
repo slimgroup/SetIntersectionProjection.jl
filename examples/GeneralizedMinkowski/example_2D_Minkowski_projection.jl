56f344c062156309e263e3d58a49fc57382c2a92
Bas <1.bas.peters@gmail.com>
2020-12-02 19:25:32 -0500

    updated an example

--- examples/GeneralizedMinkowski/example_2D_Minkowski_projection.jl
+++ examples/GeneralizedMinkowski/example_2D_Minkowski_projection.jl
@@ -1,6 +1,6 @@
 #This script illustate how to set up constraints and project a 2D model onto an intersection
 # of an intersection and a Minkowski sum of intersections of sets
-# Bas Peters, 2017
+# Bas Peters, 2019
 
 using Distributed
 @everywhere using SetIntersectionProjection
@@ -24,6 +24,9 @@ options.adjust_feasibility_rho = true
 options.Blas_active            = true
 options.maxit                  = 500
 
+options.feas_tol     = 0.001
+options.obj_tol      = 0.001
+options.evol_rel_tol = 0.00001
 set_zero_subnormals(true)
 BLAS.set_num_threads(2)
 
@@ -49,12 +52,14 @@ m         = convert(Vector{TF},vec(m))
 #define axis limits and colorbar limits
 xmax = comp_grid.d[1]*comp_grid.n[1]
 zmax = comp_grid.d[2]*comp_grid.n[2]
-vmi  = 1500
-vma  = 4500
+vmi  = 1500.0
+vma  = 4500.0
 
-#constraints for total model
+#####################################################################################################
+# approximate monotonically increasing velocity with depth + TV decomposition #######################
+
+#constraints for total model (sum of two compontents)
 constraint_sum = Vector{SetIntersectionProjection.set_definitions}()
-
 
 #bounds:
 m_min     = 1500.0
@@ -65,14 +70,14 @@ app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-
 #constraints for component 1 of the model
 constraint_c1 = Vector{SetIntersectionProjection.set_definitions}()
 
 #bounds component 1:
-m_min     = 1500f0*ones(TF,prod(comp_grid.n))
+m_min     = 1500f0.*ones(TF,comp_grid.n)
 m_max     = 4500f0.*ones(TF,comp_grid.n)
 m_max[:,1:35] .= 1500f0
+m_min     = vec(m_min)
 m_max     = vec(m_max)
 set_type  = "bounds"
 TD_OP     = "identity"
@@ -89,59 +94,63 @@ app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-
 #constraints for component 2 of the model
 constraint_c2 = Vector{SetIntersectionProjection.set_definitions}()
 
 #bounds component 2:
-m_min     = -500.0
-m_max     = 500.0
+m_min     = -1500.0
+m_max     = 1500.0
 set_type  = "bounds"
 TD_OP     = "identity"
 app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-#rank constraint component 2
+#TV constraint component 2
 m_min     = 0
-m_max     = 20
-set_type  = "rank"
-TD_OP     = "identity"
+(TV,dummy1,dummy2,dummy3) = get_TD_operator(comp_grid,"TV",options.FL)
+m_max = 0.15f0*norm(TV*vec(m),1)
+set_type  = "l1"
+TD_OP     = "TV"
 app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
+#get all projectors for sub-problems and linear operators
 (P_sub_sum,TD_OP_sum,set_Prop_sum) = setup_constraints(constraint_sum,comp_grid,options.FL)
-(P_sub_c1,TD_OP_c1,set_Prop_c1) = setup_constraints(constraint_c1,comp_grid,options.FL)
-(P_sub_c2,TD_OP_c2,set_Prop_c2) = setup_constraints(constraint_c2,comp_grid,options.FL)
+(P_sub_c1,TD_OP_c1,set_Prop_c1)    = setup_constraints(constraint_c1,comp_grid,options.FL)
+(P_sub_c2,TD_OP_c2,set_Prop_c2)    = setup_constraints(constraint_c2,comp_grid,options.FL)
 
+#pre-computations
 options.parallel  = false
 options.Minkowski = true
 (TD_OP,set_Prop,AtA,l,y) = PARSDMM_precompute_distribute_Minkowski(TD_OP_c1,TD_OP_c2,TD_OP_sum,set_Prop_c1,set_Prop_c2,set_Prop_sum,comp_grid,options)
 
+#put all projectors for sub-problems in a vector
 P_sub = deepcopy(P_sub_c1);
 append!(P_sub,P_sub_c2)
 append!(P_sub,P_sub_sum)
 
 options.zero_ini_guess=true
 
-@time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
+#project
 @time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
 @time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
 
-#plot
+
+#plot; output is component 1 (m1), component 2 (m2) stacked in a vector
 m1 = x[1:length(m)]
 m2 = x[1+length(m):end]
-ms = m1+m2;
+ms = m1+m2; #total model output
 
-figure();imshow(permutedims(reshape(m,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("model to project")
+figure();imshow(permutedims(reshape(m,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Original")
 savefig("original_model.png",bbox_inches="tight")
-figure();imshow(permutedims(reshape(m1,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (component 1)")
+figure();imshow(permutedims(reshape(m1,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (component 1, positive vertical slope)")
 savefig("projected_model_component1.png",bbox_inches="tight")
-figure();imshow(permutedims(reshape(m2,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=-1500,vmax=1500,extent=[0,  xmax, zmax, 0]); title("Projection (component 2)")
+figure();imshow(permutedims(reshape(m2,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=-1500,vmax=1500,extent=[0,  xmax, zmax, 0]); title("Projection (component 2, low TV)")
 savefig("projected_model_component2.png",bbox_inches="tight")
 figure();imshow(permutedims(reshape(ms,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (sum of components)")
-savefig("projected_model_component2.png",bbox_inches="tight")
+savefig("projected_model_component_sum.png",bbox_inches="tight")
 
 figure();
 subplot(3, 3, 3);semilogy(log_PARSDMM.r_pri)          ;title("r primal")
@@ -158,10 +167,8 @@ tight_layout()
 savefig("PARSDMM_logs.png",bbox_inches="tight")
 
 
-###############################################################################################
-# Now we try another decomposition, into a component with cardinality constraints on the vertical
-# gradient to obtain a component with 6 'units'. The other component contains the details.
-###############################################################################################
+#################################################################################################################################
+# approximate monotonically increasing velocity with depth + monotonically decreasing velocity with depth #######################
 
 #constraints for total model
 constraint_sum = Vector{SetIntersectionProjection.set_definitions}()
@@ -175,14 +182,13 @@ app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-
 #constraints for component 1 of the model
 constraint_c1 = Vector{SetIntersectionProjection.set_definitions}()
 
 #bounds component 1:
-m_min     = 1500f0*ones(TF,prod(comp_grid.n))
-m_max     = 4500f0.*ones(TF,comp_grid.n)
-m_max[:,1:35] .= 1500f0
+m_min     = 1500.0f0*ones(TF,prod(comp_grid.n))
+m_max     = 4500.0f0.*ones(TF,comp_grid.n)
+m_max[:,1:35] .= 1500.0f0
 m_max     = vec(m_max)
 set_type  = "bounds"
 TD_OP     = "identity"
@@ -190,42 +196,32 @@ app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-#cardinality constraints component 1 (vertical)
-m_min     = 0
-m_max     = 5
-set_type  = "cardinality"
+#slope constraints component 1(vertical, positive)
+m_min     = 0.0
+m_max     = 1e6
+set_type  = "bounds"
 TD_OP     = "D_z"
-app_mode  = ("fiber","z")
-custom_TD_OP = ([],false)
-push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
-
-#rank constraint component 1
-m_min     = 0
-m_max     = 20
-set_type  = "rank"
-TD_OP     = "identity"
 app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
-push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
-
+push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
 #constraints for component 2 of the model
 constraint_c2 = Vector{SetIntersectionProjection.set_definitions}()
 
 #bounds component 2:
-m_min     = -500.0
-m_max     = 500.0
+m_min     = -3000.0
+m_max     = 0.0
 set_type  = "bounds"
 TD_OP     = "identity"
 app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
 
-#rank constraint component 2
-m_min     = 0
-m_max     = 20
-set_type  = "rank"
-TD_OP     = "identity"
+#slope constraints component 1(vertical, negative)
+m_min     = -1e6
+m_max     = 0.0
+set_type  = "bounds"
+TD_OP     = "D_z"
 app_mode  = ("matrix","")
 custom_TD_OP = ([],false)
 push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
@@ -246,21 +242,20 @@ options.zero_ini_guess=true
 
 @time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
 @time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
-@time (x,log_PARSDMM) = PARSDMM(m,AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
 
 #plot
 m1 = x[1:length(m)]
 m2 = x[1+length(m):end]
 ms = m1+m2;
 
-figure();imshow(permutedims(reshape(m,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("model to project")
+figure();imshow(permutedims(reshape(m,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Original")
 savefig("original_model.png",bbox_inches="tight")
-figure();imshow(permutedims(reshape(m1,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (component 1)")
+figure();imshow(permutedims(reshape(m1,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (component 1, positive vertical slope)")
 savefig("projected_model_component1.png",bbox_inches="tight")
-figure();imshow(permutedims(reshape(m2,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=-1500,vmax=1500,extent=[0,  xmax, zmax, 0]); title("Projection (component 2)")
+figure();imshow(permutedims(reshape(m2,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=-1500,vmax=1500,extent=[0,  xmax, zmax, 0]); title("Projection (component 2, negative vertical slope)")
 savefig("projected_model_component2.png",bbox_inches="tight")
 figure();imshow(permutedims(reshape(ms,(comp_grid.n[1],comp_grid.n[2])),[2,1]),cmap="jet",vmin=vmi,vmax=vma,extent=[0,  xmax, zmax, 0]); title("Projection (sum of components)")
-savefig("projected_model_component2.png",bbox_inches="tight")
+savefig("projected_model_component_sum.png",bbox_inches="tight")
 
 figure();
 subplot(3, 3, 3);semilogy(log_PARSDMM.r_pri)          ;title("r primal")
