0f6734609f279c1bc6f55c4c79ad110bfec4cfb9
PetersBas <1.bas.peters@gmail.com>
2021-02-12 17:47:42 -0500

    added constrained nn examples

---
+++ examples/GeneralizedMinkowski/ConstrainedNeuralNetworkSegmentation_cameraman.jl
@@ -0,0 +1,428 @@
+using PyPlot
+using TestImages
+using LinearAlgebra
+using Random
+using Statistics
+using Printf
+using SetIntersectionProjection
+using InvertibleNetworks #(https://github.com/slimgroup/InvertibleNetworks.jl)
+using Flux
+import Flux.Optimise.update!
+
+#Load image
+image = testimage("cameraman")
+image = convert(Array{Float32,2},image)
+image = image[1:4:end,1:4:end]
+image = repeat(image,outer=[1,1,9,1]) #make image 9 channels for the invertible network
+
+
+################## set up Generalized Minkowski constriants################
+################## set up Generalized Minkowski constriants################
+
+#describe a 'body' as the sum of monotonically increasing and decreasing functions
+#also use cardinality constraint to indicate the minimum and maximum size of the object
+#include bound constraints
+
+#PARSDMM (ADMM-based algorithm that computes the projection) options:
+options=PARSDMM_options()
+options.FL=Float32
+options.adjust_gamma           = true
+options.adjust_rho             = true
+options.adjust_feasibility_rho = true
+options.Blas_active            = true
+options.maxit                  = 700
+
+#accuracy, these are very conservative choices, to make things faster you can change these values
+options.feas_tol     = 0.001
+options.obj_tol      = 0.001
+options.evol_rel_tol = 0.0001
+set_zero_subnormals(true)
+BLAS.set_num_threads(4)
+
+mutable struct compgrid #set up computational grid
+  d :: Tuple
+  n :: Tuple
+end
+
+P         = Vector{Any}(undef, 1) #allocate vector of Projectors (just one in this case because there is one examle)
+P[1]      = Vector{Any}(undef, 2)
+n         = size(image[:,:,1,1])
+comp_grid = compgrid((1.0,1.0),(n[1], n[2]))
+
+for j=1:2 #loop over active output channels (classes)
+  if j==1
+    ######## Channel 1 (anomaly) ############################
+
+    #constraints for total model
+    constraint_sum = Vector{SetIntersectionProjection.set_definitions}()
+
+    #cardinality:
+    m_min     = 0
+    m_max     = round(Int,0.5*prod(n[1:2]))
+    set_type  = "cardinality"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #bounds:
+    m_min     = zeros(Float32,n[1],n[2])
+    m_max     = ones(Float32,n[1],n[2])
+    m_min = vec(m_min)
+    m_max = vec(m_max)
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #constraints for component 1 of the model
+    constraint_c1 = Vector{SetIntersectionProjection.set_definitions}()
+
+    #bounds component 1:
+    m_min     = -2f0
+    m_max     = 2f0
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 1(vertical)
+    m_min     = 0.0
+    m_max     = 1e6
+    set_type  = "bounds"
+    TD_OP     = "D_z"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    # #slope constraints component 1(horizontal)
+    m_min     = 0.0
+    m_max     = 1e6
+    set_type  = "bounds"
+    TD_OP     = "D_x"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #constraints for component 2 of the model
+    constraint_c2 = Vector{SetIntersectionProjection.set_definitions}()
+
+    #bounds component 2:
+    m_min     = -2.0f0
+    m_max     = 2.0f0
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 2(vertical)
+    m_min     = -1e6
+    m_max     = 0.0
+    set_type  = "bounds"
+    TD_OP     = "D_z"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    # #slope constraints component 2(horizontal)
+    m_min     = -1e6
+    m_max     = 0.0
+    set_type  = "bounds"
+    TD_OP     = "D_x"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+
+    (P_sub_sum,TD_OP_sum,set_Prop_sum) = setup_constraints(constraint_sum,comp_grid,options.FL)
+    (P_sub_c1,TD_OP_c1,set_Prop_c1) = setup_constraints(constraint_c1,comp_grid,options.FL)
+    (P_sub_c2,TD_OP_c2,set_Prop_c2) = setup_constraints(constraint_c2,comp_grid,options.FL)
+
+    options.parallel  = false
+    options.Minkowski = true
+    (TD_OP,set_Prop,AtA,l,y) = PARSDMM_precompute_distribute_Minkowski(TD_OP_c1,TD_OP_c2,TD_OP_sum,set_Prop_c1,set_Prop_c2,set_Prop_sum,comp_grid,options)
+
+    P_sub = deepcopy(P_sub_c1);
+    append!(P_sub,P_sub_c2)
+    append!(P_sub,P_sub_sum)
+
+    options.zero_ini_guess=true
+
+    function temp_func1(x,AtA,TD_OP,set_Prop,P_sub,comp_grid,options)
+        sx = size(x)
+        (x,log_PARSDMM) = PARSDMM(vec(x),AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
+        x = x[1:Int(length(x)/2)]+x[1+Int(length(x)/2):end]
+        x = reshape(x,sx)
+        return x
+    end
+    P[1][1] = x -> temp_func1(x,AtA,TD_OP,set_Prop,P_sub,comp_grid,options)
+
+  elseif j==2
+    ######## Channel 2 (background) ############################
+
+    #constraints for total model
+    constraint_sum = Vector{SetIntersectionProjection.set_definitions}()
+
+    #cardinality:
+    m_min     = 0
+    m_max     = round(Int,0.90*prod(n[1:2]))
+    set_type  = "cardinality"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #bounds:
+    m_min     = zeros(Float32,n[1],n[2])
+    m_max     = ones(Float32,n[1],n[2])
+    m_min = vec(m_min)
+    m_max = vec(m_max)
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_sum, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #constraints for component 1 of the model
+    constraint_c1 = Vector{SetIntersectionProjection.set_definitions}()
+
+    #bounds component 1:
+    m_min     = -2f0
+    m_max     = 2f0
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 1(vertical)
+    m_min     = 0.0
+    m_max     = 1e6
+    set_type  = "bounds"
+    TD_OP     = "D_z"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 1(horizontal)
+    m_min     = 0.0
+    m_max     = 1e6
+    set_type  = "bounds"
+    TD_OP     = "D_x"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c1, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #constraints for component 2 of the model
+    constraint_c2 = Vector{SetIntersectionProjection.set_definitions}()
+
+    #bounds component 2:
+    m_min     = -2.0
+    m_max     = 2.0
+    set_type  = "bounds"
+    TD_OP     = "identity"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 2(vertical)
+    m_min     = -1e6
+    m_max     = 0.0
+    set_type  = "bounds"
+    TD_OP     = "D_z"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    #slope constraints component 2(horizontal)
+    m_min     = -1e6
+    m_max     = 0.0
+    set_type  = "bounds"
+    TD_OP     = "D_x"
+    app_mode  = ("matrix","")
+    custom_TD_OP = ([],false)
+    push!(constraint_c2, set_definitions(set_type,TD_OP,m_min,m_max,app_mode,custom_TD_OP))
+
+    (P_sub_sum,TD_OP_sum,set_Prop_sum) = setup_constraints(constraint_sum,comp_grid,options.FL)
+    (P_sub_c1,TD_OP_c1,set_Prop_c1) = setup_constraints(constraint_c1,comp_grid,options.FL)
+    (P_sub_c2,TD_OP_c2,set_Prop_c2) = setup_constraints(constraint_c2,comp_grid,options.FL)
+
+    options.parallel  = false
+    options.Minkowski = true
+    (TD_OP,set_Prop,AtA,l,y) = PARSDMM_precompute_distribute_Minkowski(TD_OP_c1,TD_OP_c2,TD_OP_sum,set_Prop_c1,set_Prop_c2,set_Prop_sum,comp_grid,options)
+
+    P_sub = deepcopy(P_sub_c1);
+    append!(P_sub,P_sub_c2)
+    append!(P_sub,P_sub_sum)
+
+    options.zero_ini_guess=true
+
+    function temp_func2(x,AtA,TD_OP,set_Prop,P_sub,comp_grid,options)
+        sx = size(x)
+        (x,log_PARSDMM) = PARSDMM(vec(x),AtA,TD_OP,set_Prop,P_sub,comp_grid,options);
+        x = x[1:Int(length(x)/2)]+x[1+Int(length(x)/2):end]
+        x = reshape(x,sx)
+        return x
+    end
+
+    P[1][2] = x -> temp_func2(x,AtA,TD_OP,set_Prop,P_sub,comp_grid,options)
+    end
+  end
+
+################## End setup Generalized Minkowski constraints ################
+
+#set up neural network
+
+#12 layers with 9 channels each for a reversible hyperbolic network
+architecture = ((0, 9), (0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9),(0, 9))
+
+k = 3   # kernel size
+s = 1   # stride
+p = 1   # padding
+n_chan_in = size(image,3)
+(nx, ny)  = size(image)[1:2]
+α         = 0.3^2
+batchsize = 1
+HN = H = NetworkHyperbolic(nx, ny,n_chan_in, batchsize, architecture; α)
+
+#squared distance function to a constraint set
+function Dist2ToSet(input,P,active_channels,example_ind)
+
+  #initialize some stuff
+  size_input = size(input)
+  pen_grad   = zeros(Float32,size(input))
+  pen_value  = 0f0
+
+  #use softmax of final network state for constraining
+  #the output for segmentation problems (don't use softmax for non-linear regression)
+  input      = softmax(input[:,:,active_channels,:],dims=3)
+
+  for j in active_channels #loop over channels, each channel has a (different) corresponding projector
+    input_slice        = input[:,:,j,1]
+    pen_value_slice    = 0f0 #
+    P_input            = P[example_ind][j](input_slice) #project onto constraint set (can be an intersection)
+    pen_value_slice    = 0.5f0*norm(vec(P_input)-vec(input_slice))^2 #squared point-to-set distance functino
+    pen_grad[:,:,j,1] .= input_slice .- P_input # gradient of point-to-set distance function
+
+    pen_value = pen_value + pen_value_slice #accumulate penalty value (loss) over channels
+  end #end channels loop
+
+  #at this point,the softmax of the network output has been projected onto the intersection. Now we need to 'complete' the gradient
+  #via the last part of the chain-rule for distance-function-squared and softmax
+  pen_grad[:,:,active_channels,1] .= ∇softmax(pen_grad[:,:,active_channels,1], input[:,:,active_channels,1]; dims=3)
+
+  return pen_value, pen_grad
+end
+
+#loss function
+function loss(HN, X0, X1,P,active_channels,flip_dims)
+
+  #(optionally) augment
+  X0_augment = deepcopy(X0)
+  for i in flip_dims
+    if rand() > 0.5
+      X0_augment = reverse(X0_augment,dims=i)
+    end
+  end
+
+  #forward prop through the network
+  Y_curr, Y_new, lgdet = HN.forward(X0_augment, X0_augment)
+
+  #copmute squared distance function value and gradient
+  dc2,dc2_grad = Dist2ToSet(Y_new,P,active_channels,1)
+
+  #backpropagation through the network to obtain the gradient w.r.t. the network parameters
+  ΔY_curr  = dc2_grad
+  ΔY_new = zeros(Float32,size(dc2_grad))
+  ΔY_curr, ΔY_new, Y_curr, Y_new = HN.backward(ΔY_curr, ΔY_new, Y_curr, Y_new)
+
+  return dc2
+end
+
+
+#Test reversibility: neural network forward prop -> inverse prop
+X_prev, X_curr, lgdet = HN.forward(deepcopy(image),deepcopy(image))
+Y_curr_rec, Y_new_rec = HN.inverse(X_prev, X_curr)
+norm(vec(Y_curr_rec)-vec(image))./norm(vec(image)) #should be very small number
+
+#training loop function
+function train(HN,X0,X1,P,active_channels,flip_dims,maxiter,opt)
+  fval        = zeros(Float32, maxiter)
+
+  for j=1:maxiter
+
+    # Evaluate objective and gradients
+    fval[j] = loss(HN, X0, X1,P,active_channels,flip_dims)
+    mod(j, 1) == 0 && (print("Iteration: ", j, "; f = ", fval[j], "\n"))
+
+    # Update network params
+    counter = 1
+    for p in get_params(HN)
+      update!(opt, p.data, p.grad)
+    end
+    clear_grad!(HN)
+
+  end
+
+  return fval
+end
+
+#training and optimization settings
+maxiter         = 40
+opt             = Flux.ADAM(1f-2)
+active_channels = [1,2]
+flip_dims       = []
+
+fval = train(HN,image,image,P,active_channels,flip_dims,maxiter,opt)
+
+fval_total = fval
+
+#plot results
+figure(figsize=(3,6));semilogy(fval_total);title("Squared distance-to-set");
+xlabel("Iteration number");
+savefig("loss.png")
+
+#Plot input data
+figure(figsize=(4,4))
+imshow(-image[:,:,1,1],cmap="Greys")
+title("Cameraman")
+savefig("data.png")
+
+#predict using trained network
+~, prediction, ~ = HN.forward(deepcopy(image),deepcopy(image))
+prediction[:,:,1:2,1] .= softmax(prediction[:,:,1:2,1],dims=3);
+
+#project results on Generalized Minkowski set
+project_channel_1 = P[1][1](vec(deepcopy(prediction[:,:,1,1])));
+project_channel_2 = P[1][2](vec(deepcopy(prediction[:,:,2,1])));
+project_channel_1 = reshape(project_channel_1,comp_grid.n[1],comp_grid.n[2])
+project_channel_2 = reshape(project_channel_2,comp_grid.n[1],comp_grid.n[2])
+
+vmi = 0.0
+vma = 1.0
+figure(figsize=(8,4))
+subplot(1,2,1);imshow(Array(project_channel_1)[3:end-2,3:end-2]);PyPlot.title("Prediction - channel 1");colorbar()
+subplot(1,2,2);imshow(Array(project_channel_2)[3:end-2,3:end-2]);PyPlot.title("prediction - channel 2");colorbar()
+savefig("prediction_cameraman_Channels.png")
+
+#thresholded prediction
+pred_thres = zeros(Int,size(prediction)[1:2])
+pos_inds   = findall(project_channel_1 .> project_channel_2)
+pred_thres[pos_inds] .= 1
+
+figure(figsize=(4,4));
+imshow(Array(pred_thres)[3:end-2,3:end-2],vmin=vmi,vmax=vma);PyPlot.title("Prediction");
+savefig("prediction_cameraman.png")
+
+#Plot data+prediction
+figure(figsize=(4,4))
+imshow(Array(pred_thres)[2:end-1,2:end-1])
+imshow(-image[2:end-1,2:end-1,3],alpha=0.6,cmap="Greys")
+ax = gca()
+ax[:xaxis][:set_visible](false)
+ax[:yaxis][:set_visible](false)
+title("Image+Prediction")
+savefig("data_plus_pred.png")
